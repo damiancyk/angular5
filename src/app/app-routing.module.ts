6c22edd923a09b7d7e47e1c0bc0ffe5a599d3379
Damian Pieta <damiancyk@gmail.com>
2017-11-29 16:04:26 +0100

    Animations and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';     // Add this
+import { AboutComponent } from './about/about.component';  // Add this
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'about/:id',             // Add /:id here
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+constructor(private route: ActivatedRoute, private router: Router) { }
+
+  sendMeHome() {
+      this.router.navigate(['']);
+    }
+}
